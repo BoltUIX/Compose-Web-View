1222df8d0400694b8468cef77422efe3e701d62c
BoltUIX <boltuix@gmail.com>
2022-07-29 13:01:16 +0530

    Compose WebView Part 7 | Hide elements from web view Compose WebView Part 8 | Navigation Go To Previous Page. Compose WebView Part 9 | Removes or Stop Ad in web

--- README.md
+++ README.md
@@ -1,18 +1,36 @@
 # Web-view-jetpack-compose-2022
+Compose WebView Part 9 | Removes or Stop Ad in web
+https://www.boltuix.com/2022/07/compose-webview-part-9-removes-or-stop.html
+<img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEheea_22IwA7Q2huidvshaQWkHIhBXdb-Rf-VX-HwTp4Fu70dUvOPxGq9-mBxZyqIsfkPqCbutef2MzDWVFjH0maA4rWGJElomrBx_JrSQNGrbbEJSdRVkm-XfH0WuRVLOnwVN_-1RI4Pk9-QBcam--I6brpGHDl2MjMevFySw6Wg6mhhbwxCQeYzGi/s1280/remove%20ads.jpg" alt="Compose" width="100%">
+
+Compose WebView Part 8 | Navigation Go To Previous Page
+https://www.boltuix.com/2022/07/compose-webview-part-8-navigation-go-to.html
+<img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgxxYuqRMa6K9Nw-HtYFH113FF3Gx6Eb-HVNzAOJK9Gy71f8Wu3AonHZl_Mf0VVIuotTlGen8UEAw348gRgQwoCJpKIshPKUbgZQ4EOSJK84gi9ByWEnDtGgrGF1XiivQax34faDtmhSt7ZbE3FYGiCHcNhbN9HlJN0AZ9UJEsMFNQBIZ7S4oyIPWy7/s16000/navigation.jpg" alt="Compose" width="100%">
+
+Compose WebView Part 7 | Hide elements from web view
+https://www.boltuix.com/2022/07/compose-webview-part-7-hide-elements.html
+<img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiZjHdoRncYbYRLnbwEQ_HkHu2JK6VObAnY8IZfksa8oNXKFgxC27aSmb8g-1fkt9HbZi29DSMEr9pGV1uxrTrGv1L83q4PEkEQ4DuPlklDiyog-_c-2wKVA5tN1341GDEogo3qeUwyN4LtgoybPchtBEKi3Zf0MpA3WDrbQUuLef85HiMcabmloFTc/s16000/compose%20ppt.jpg" alt="Compose" width="100%">
+
 Compose WebView Part 6 | Bind JavaScript code to Android code
+https://www.boltuix.com/2022/07/compose-webview-part-6-bind-javascript.html
 <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh49aLI_pjND2Y0YdE5k7-hS8AWKHm_XQTEJcz1pu7KK8YPoOoWDsygKjCNBiQIXC7qxsE_pbPNxT9HL6e_tXDmijzmZHyxtj_ToJsr95c_batCBTFPKyUyLxN2BjMRY4wM-xdlG3ADc17GF5LAmJturgUEqBslA239ERaY98AU1GAk5lLv52KgBjaS/s16000/bind.jpg" alt="Compose" width="100%">
 
 Compose WebView Part 5 | Should Override URL Loading
+https://www.boltuix.com/2022/07/compose-webview-part-5-should-override.html
 <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh6pMXcqkM1nypmP1wz9JXDdS9ZX4CnZV_cwDOEmIcFIkXU6pXlUqhCD3G4_nFQcqb_V561lDUOno2tBCQUWnK3kJJG86ygzYIEfKX1ULfHdX0mLIy_v9vJ6sKjvSElCfy4JJVqmXz2kua5ddgLBtqi5NGewNLHNjwIJ34vyHwsUBiVLuC5CfUFjOcE/s16000/webview.jpg" alt="Compose" width="100%">
 
 Compose WebView Part 4 | OFFLINE
+https://www.boltuix.com/2022/07/compose-webview-part-4-offline.html
 <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjS8kr7l9vZlOTvgnqqvyTsmkceFxx0ePugvpQSOxsc-Vfl1YUXcSPa-avE20awqWk_3fnhSOPlCtM4P0Z7dJDpR7wADZyGa1SlTEfQrFUd8R1-_fn7vfk_hS_j_QMCAFzwBCdmFytXxyvMKnr8vcJ3uFo79s69G8Xf81fNDLhVjtLmuGZc_aUF3v4c/s16000/compose%20ppt.jpg" alt="Compose" width="100%">
 
 Compose WebView Part 3 | Handle configuration changes
+https://www.boltuix.com/2022/07/compose-webview-part-3-handle.html
 <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjQw6tjwCK_k-EJjHvDBfu9TV9Vh3JJgR44g3pTkd33STJhtkrq559x7wwxSALS5GyW8CSCs4M42ULTInZaKciRmF8UiP2i0pFlexgaFVw4UCOXTmYbFdcoAErDMtRwg46nOtUNzZTw7xbS_aRaPFt079myrL900RSJT-SKpGU4h0fzlev9YGEelR3q/s16000/web%20view%20compose.jpg" alt="Compose" width="100%">
 
 Compose WebView Part 2 Play Video | JS setting
+https://www.boltuix.com/2022/07/compose-webview-part-2-play-video.html
 <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj7GnE3XN1Dvj0uhe7NOVihIvRNLBxuDlYKX5vkUX_M1AxilHV6J8AwdrIXGhgVTRWXNAbzW_42amW3y42uLWuYt4hK6MX5uddO6XYecf_S4ehY7XIDZggd2Yeo5BuqzIjg1jiGwbi1hr76Ml30kMMlIHepVrmFD9ub6barhYeGPsZaJnwDvJYfaKzC/s16000/compose%20web%20play%20video.jpg" alt="Compose" width="100%">
 
 Compose WebView Part 1 Load URL
+https://www.boltuix.com/2022/07/compose-webview-part-1-load-url.html
 <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgfRX1kX5e658_hr97AZSNEFFEFWl7PukN6dcHrn-HzLu0xpPJ4d1WLTjS-LiX-faPo2Xh33A1rAe6G4pePW5ZshzEHWGSbfhlv-qjo9AvF4wRPrr14InAc88NEosrLji8PgBljIcfe2Is8sP81diMTLGwbd45WxjTt-WuQMU7WB51AKQhDHsybeBvf/s16000/compose%20web1.jpg" alt="Compose" width="100%">
--- app/src/main/java/com/blogspot/boltuix/MainActivity.kt
+++ app/src/main/java/com/blogspot/boltuix/MainActivity.kt
@@ -3,32 +3,47 @@ package com.blogspot.boltuix
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.res.Configuration
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.util.Log
 import android.view.ViewGroup
-import android.webkit.JavascriptInterface
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.webkit.*
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import java.io.BufferedReader
+import java.io.ByteArrayInputStream
+import java.io.IOException
+import java.io.InputStreamReader
 
+var loadURL = "https://www.boltuix.com/"
 
 class MainActivity : ComponentActivity() {
-
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
             WebViewPage("https://www.boltuix.com/")
             //WebViewPage("file:///android_asset/shop.html") //OFFLINE
         }
@@ -36,78 +51,206 @@ class MainActivity : ComponentActivity() {
 }
 
 
-
-
+@OptIn(ExperimentalComposeUiApi::class)
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
 fun WebViewPage(url: String){
+
+    val openFullDialogCustom = remember { mutableStateOf(false) }
+    if (openFullDialogCustom.value) {
+
+        // Dialog function
+        Dialog(
+            onDismissRequest = {
+                openFullDialogCustom.value = false
+            },
+            properties = DialogProperties(
+                usePlatformDefaultWidth = false // experimental
+            )
+        ) {
+            Surface(modifier = Modifier.fillMaxSize()) {
+
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+
+
+
+                    Image(
+                        painter = painterResource(id = R.drawable.logo),
+                        contentDescription = null,
+                        contentScale = ContentScale.Fit,
+                        modifier = Modifier
+                            .height(200.dp)
+                            .fillMaxWidth(),
+
+                        )
+
+                    Spacer(modifier = Modifier.height(20.dp))
+                    //.........................Text: title
+                    Text(
+                        text = "Loading...",
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .padding(top = 20.dp)
+                            .fillMaxWidth(),
+                        letterSpacing = 2.sp,
+                        fontWeight = FontWeight.Bold,
+                        style = MaterialTheme.typography.titleLarge,
+                        color = MaterialTheme.colorScheme.primary,
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    //.........................Text : description
+                    Text(
+                        text = "Please wait",
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .padding(top = 10.dp, start = 25.dp, end = 25.dp)
+                            .fillMaxWidth(),
+                        letterSpacing = 3.sp,
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.primary,
+                    )
+                    //.........................Spacer
+                    Spacer(modifier = Modifier.height(24.dp))
+
+                }
+
+            }
+        }
+
+    }
+    //..........................................................................
+
+
+
+
+    val context  = LocalContext.current
+
+    //.................................................
+    // Compose WebView Part 9 | Removes or Stop Ad in web
+    val adServers = StringBuilder()
+    var line: String? = ""
+    val inputStream = context.resources.openRawResource(R.raw.adblockserverlist)
+    val br = BufferedReader(InputStreamReader(inputStream))
+    try {
+        while (br.readLine().also { line = it } != null) {
+            adServers.append(line)
+            adServers.append("\n")
+        }
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
+
+
+
+    var backEnabled by remember { mutableStateOf(false) }
+    var webView: WebView? = null
+
+
     val mutableStateTrigger = remember { mutableStateOf(false) }
     val infoDialog = remember { mutableStateOf(false) }
 
-    val context  = LocalContext.current
+
 
     //The Configuration object represents all of the current configurations, not just the ones that have changed.
     val configuration = LocalConfiguration.current
     when (configuration.orientation) {
         Configuration.ORIENTATION_LANDSCAPE -> {
-            Toast.makeText(context, "landscape", Toast.LENGTH_SHORT).show()
+            // Toast.makeText(context, "landscape", Toast.LENGTH_SHORT).show()
         }
         else -> {
-            Toast.makeText(context, "portrait", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(context, "portrait", Toast.LENGTH_SHORT).show()
         }
     }
 
-
     // Adding a WebView inside AndroidView
     // with layout as full screen
-    AndroidView(factory = {
-        WebView(it).apply {
-            layoutParams = ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT
-            )
-            webViewClient = WebViewClient()
+    AndroidView(
+        factory = {
+            WebView(it).apply {
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                webViewClient = WebViewClient()
 
-            // to play video on a web view
-            settings.javaScriptEnabled = true
+                // to play video on a web view
+                settings.javaScriptEnabled = true
 
-            // to verify that the client requesting your web page is actually your Android app.
-            settings.userAgentString = System.getProperty("http.agent") //Dalvik/2.1.0 (Linux; U; Android 11; M2012K11I Build/RKQ1.201112.002)
+                // to verify that the client requesting your web page is actually your Android app.
+                settings.userAgentString = System.getProperty("http.agent") //Dalvik/2.1.0 (Linux; U; Android 11; M2012K11I Build/RKQ1.201112.002)
 
-            settings.useWideViewPort = true
+                settings.useWideViewPort = true
 
 
-            // Bind JavaScript code to Android code
-            addJavascriptInterface(WebAppInterface(context,infoDialog), "Android")
+                // Bind JavaScript code to Android code
+                addJavascriptInterface(WebAppInterface(context,infoDialog), "Android")
 
-            // Compose WebView Part 5 | Should Override URL Loading
-            webViewClient = object : WebViewClient() {
-                @Deprecated("Deprecated in Java")
-                override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-                    if (url.contains("facebook.com")) {
-                         mutableStateTrigger.value = true
-                         Toast.makeText(context, "Custom Action", Toast.LENGTH_SHORT).show()
-                        return true
+
+                webViewClient = object : WebViewClient() {
+
+
+                    override fun onReceivedError(view: WebView?, request: WebResourceRequest?, error: WebResourceError?) {
+                        super.onReceivedError(view, request, error)
+                        Log.d("test001","error")
+                        loadURL = "file:///android_asset/404.html"
+                        mutableStateTrigger.value = true
+
                     }
-                    else{
-                        view.loadUrl(url)
+
+                    override fun shouldInterceptRequest(view: WebView, request: WebResourceRequest): WebResourceResponse? {
+                        val empty = ByteArrayInputStream("".toByteArray())
+                        val kk5 = adServers.toString()
+                        if (kk5.contains(":::::" + request.url.host))
+                            return WebResourceResponse("text/plain", "utf-8", empty)
+                        return super.shouldInterceptRequest(view, request)
                     }
-                    return false
+
+
+                    override fun onPageStarted(view: WebView, url: String?, favicon: Bitmap?) {
+                        openFullDialogCustom.value = true
+                        backEnabled = view.canGoBack()
+                    }
+
+                    // Compose WebView Part 7 | Hide elements from web view
+                    override fun onPageFinished(view: WebView?, url: String?) {
+                        super.onPageFinished(view, url)
+                        openFullDialogCustom.value = false
+                        removeElement(view!!)
+                    }
+
+                    // Compose WebView Part 5 | Should Override URL Loading
+                    @Deprecated("Deprecated in Java")
+                    override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                        if (url.contains("facebook.com")) {
+                            loadURL = "https://www.instagram.com/boltuix/"
+                            mutableStateTrigger.value = true
+                            Toast.makeText(context, "Custom Action", Toast.LENGTH_SHORT).show()
+                            return true
+                        }
+                        else{
+                            view.loadUrl(url)
+                        }
+                        return false
+                    }
                 }
+
+                loadUrl(url)
+                webView = this
             }
-
-
-            loadUrl(url)
-
-
-        }
-    }, update = {
-        it.loadUrl(url)
-    })
+        }, update = {
+            webView = it
+            // it.loadUrl(url)
+        })
 
 
     if (mutableStateTrigger.value) {
-        WebViewPage("https://www.instagram.com/boltuix/")
+        // WebViewPage("https://www.instagram.com/boltuix/")
+         WebViewPage(loadURL)
     }
     if (infoDialog.value) {
         InfoDialog(
@@ -119,7 +262,30 @@ fun WebViewPage(url: String){
             }
         )
     }
+
+
+    BackHandler(enabled = backEnabled) {
+        removeElement(webView!!)
+        webView?.goBack()
+    }
+
 }
+
+fun removeElement(webView: WebView) {
+
+    // hide element by id
+    webView.loadUrl("javascript:(function() { document.getElementById('blog-pager').style.display='none';})()");
+
+    // we can also hide class name
+    webView.loadUrl("javascript:(function() { document.getElementsByClassName('btn')[0].style.display='none';})()")
+    webView.loadUrl("javascript:(function() { document.getElementsByClassName('btn')[1].style.display='none';})()")
+    webView.loadUrl("javascript:(function() { document.getElementsByClassName('btn')[2].style.display='none';})()")
+    webView.loadUrl("javascript:(function() { document.getElementsByClassName('btn')[3].style.display='none';})()")
+    webView.loadUrl("javascript:(function() { document.getElementsByClassName('btn')[4].style.display='none';})()")
+    webView.loadUrl("javascript:(function() { document.getElementsByClassName('btn')[5].style.display='none';})()")
+    webView.loadUrl("javascript:(function() { document.getElementsByClassName('btn')[6].style.display='none';})()")
+}
+
 
 /** Instantiate the interface and set the context  */
 class WebAppInterface(private val mContext: Context, var infoDialog: MutableState<Boolean>) {
